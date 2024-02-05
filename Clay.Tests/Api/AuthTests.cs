e20c24540b397166d3a4a02804c04295bf3e69ea
Ahmed Sayed <ahmedsselim805@gmail.com>
2024-02-05 15:37:04 +0200

    Create unit tests for authentication

--- Clay.Tests/Api/AuthTests.cs
+++ Clay.Tests/Api/AuthTests.cs
@@ -1,31 +1,93 @@
-﻿namespace Clay.Tests.Api
+﻿using Clay.Api.Controllers;
+using Clay.Api.Requests;
+using Clay.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Clay.Tests.Api
 {
     [TestClass]
     public class AuthTests
     {
+        readonly private Mock<IAuthenticationService> _authenticationServiceMock;
+        readonly private AuthController _authController;
+
+        public AuthTests()
+        {
+            _authenticationServiceMock = new Mock<IAuthenticationService>();
+            _authController = new AuthController(_authenticationServiceMock.Object);
+        }
+
         [TestMethod]
-        public void TestUserLoginInvalidUsername()
+        public async Task TestUserLoginInvalidUsername()
         {
-            throw new NotImplementedException();
-        }
-        public void TestUserLoginInvalidPassword()
-        {
-            throw new NotImplementedException();
+            // Arrange
+            var invalidUsernameRequest = new LoginRequest { username = "invalidUsername", password = "validPassword" };
+            _authenticationServiceMock.Setup(x => x.AuthenticateUser(It.IsAny<string>(), It.IsAny<string>()))
+                            .ReturnsAsync((string)null);
+
+            // Act
+            var result = await _authController.login(invalidUsernameRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(UnauthorizedObjectResult));
         }
 
-        public void TestUserLogin()
+        [TestMethod]
+        public async Task TestUserLoginInvalidPassword()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var invalidPasswordRequest = new LoginRequest { username = "validUsername", password = "invalidPassword" };
+            _authenticationServiceMock.Setup(x => x.AuthenticateUser(It.IsAny<string>(), It.IsAny<string>()))
+                            .ReturnsAsync((string)null);
+
+            // Act
+            var result = await _authController.login(invalidPasswordRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(UnauthorizedObjectResult));
         }
 
-        public void TestUserLoginMissingUsername()
+        [TestMethod]
+        public async Task TestUserLogin()
         {
-            throw new NotImplementedException();
+            var token = "MockToken";
+            // Arrange
+            var validRequest = new LoginRequest {username = "validUsername", password = "validPassword" };
+            _authenticationServiceMock.Setup(x => x.AuthenticateUser(It.IsAny<string>(), It.IsAny<string>()))
+                            .ReturnsAsync(token);
+
+            // Act
+            var result = await _authController.login(validRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
         }
 
-        public void TestUserLoginMissingPassword()
+        [TestMethod]
+        public async Task TestUserLoginMissingUsername()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var missingUsernameRequest = new LoginRequest { password = "validPassword" };
+
+            // Act
+            var result = await _authController.login(missingUsernameRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(UnauthorizedObjectResult));
+        }
+
+        [TestMethod]
+        public async Task TestUserLoginMissingPassword()
+        {
+            // Arrange
+            var missingPasswordRequest = new LoginRequest { username = "validUsername" };
+
+            // Act
+            var result = await _authController.login(missingPasswordRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(UnauthorizedObjectResult));
         }
     }
 }
